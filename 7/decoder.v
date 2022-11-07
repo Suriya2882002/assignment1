4d84fc85b912ae0536e43e1d8ceb21dff062a09d
Suriya2882002 <112601117+Suriya2882002@users.noreply.github.com>
2022-11-07 09:58:27 +0530

    Create decoder.v

---
+++ 7/decoder.v
@@ -0,0 +1,18 @@
+module dec(in, out, sel);
+
+input in;
+input [1:0] sel; 
+
+output reg  [3:0] out;
+
+always @(*)
+begin
+	case(sel)
+		2'b00: out[0] = in;
+		2'b01: out[1] = in;
+		2'b10: out[2] = in;
+		2'b11: out[3] = in;
+	endcase
+end
+
+endmodule
