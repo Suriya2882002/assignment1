412110681cf9d9a4d116a175be38dd2afd308255
Suriya2882002 <112601117+Suriya2882002@users.noreply.github.com>
2022-11-07 10:07:54 +0530

    Create 4bit_ALU.v

---
+++ 10) ALU/4bit_ALU.v
@@ -0,0 +1,35 @@
+module four_bit_alu (a, b,  out, of, sel);
+
+input wire [3:0] a;
+input wire [3:0] b;
+
+output wire [3:0] out;
+output wire of;
+
+input wire  [1:0] sel;
+
+reg [4:0] interout;
+reg overflow;
+
+always @(*)
+begin
+	case(sel)
+
+		2'b00: begin
+			interout = a|b;
+		end
+
+		2'b01: begin
+			interout = a&b;
+		end
+		
+		2'b10: begin
+			interout = a+b;
+			overflow = interout[4];
+		end
+
+		2'b11: begin
+			interout = a-b;
+		end
+	endcase
+end
